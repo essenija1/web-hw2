5e6af56841be28bc5064e75b9130949aba322664
essenija1 <80848058+essenija1@users.noreply.github.com>
2021-12-05 14:00:32 +0300

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  entry: './loadscript/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      }
+    ]
+},
+plugins: [ 
+    new ExtractTextPlugin(
+      {filename: 'timer.css'}
+    ),
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: './loadscript/timer.html',
+      filename: 'timer.html'
+    })
+  ]
+};
