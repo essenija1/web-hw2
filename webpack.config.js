32f3adbc3598c3637e17b3da65e7c6eba89b0018
essenija1 <80848058+essenija1@users.noreply.github.com>
2021-12-10 19:26:47 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.scss$/,
+        test: /\.css$/,
         use: ExtractTextPlugin.extract({
           fallback: 'style-loader',
           use: ['css-loader', 'sass-loader']
