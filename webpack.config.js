76056aedecbc3e03e2975cea6a712c62fac6da33
essenija1 <80848058+essenija1@users.noreply.github.com>
2021-12-10 19:43:37 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,14 @@
 const path = require('path');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: './loadscript/app.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'app.js'
-  },
+  plugins: [new MiniCssExtractPlugin()],
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader']
-        })
-      }
-    ]
-},
-plugins: [ 
-    new ExtractTextPlugin(
-      {filename: 'timer.css'}
-    ),
-    new HtmlWebpackPlugin({
-      inject: false,
-      hash: true,
-      template: './loadscript/timer.html',
-      filename: 'timer.html'
-    })
-  ]
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
 };
